424b795dcbf032f0fb4062578c2e284824056de1
LiHua <1985390347@qq.com>
2021-11-01 12:29:13 +0800

    增加了关注功能

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -6,6 +6,9 @@ public class RedisKeyUtil {
     private static final String SPLIT = ":";
     private static final String PREFIX_ENTITY_LIKE = "like:entity";
     private static final String PREFIX_USER_LIKE = "like:user";
+    private static final String PREFIX_FOLLOWEE = "followee";
+    private static final String PREFIX_FOLLOWER = "follower";
+
 
     /**
      * 评论评论entityId是该条评论的entityId，回复的entityId是评论数据库的自增字段id
@@ -25,6 +28,19 @@ public class RedisKeyUtil {
     // like:user:userId -> int
     public static String getUserLikeKey(int userId) {
         return PREFIX_USER_LIKE + SPLIT + userId;
+    }
+
+    // 某个用户关注的实体
+    // followee:userId:entityType -> zset(entityId,now)
+    // 以当前时间进行排序
+    public static String getFolloweeKey(int userId, int entityType) {
+        return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
+    }
+
+    // 某个实体拥有的粉丝
+    // follower:entityType:entityId -> zset(userId,now)
+    public static String getFollowerKey(int entityType, int entityId) {
+        return PREFIX_FOLLOWER + SPLIT + entityType + SPLIT + entityId;
     }
 
 }
