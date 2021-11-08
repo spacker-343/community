2ebd08cc0631cbd0266aa6d598eb31c6c7dbefb2
LiHua <1985390347@qq.com>
2021-11-08 12:42:19 +0800

    任务执行和调度

--- src/main/java/com/nowcoder/community/service/AlphaService.java
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -1,5 +1,9 @@
 package com.nowcoder.community.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -7,6 +11,8 @@ import javax.annotation.PreDestroy;
 
 @Service
 public class AlphaService {
+
+    private static final Logger logger = LoggerFactory.getLogger(AlphaService.class);
 
     //在构造方法之前调用
     @PostConstruct
@@ -21,5 +27,16 @@ public class AlphaService {
     @PreDestroy
     public void destroy() {
         System.out.println("销毁AlphaService");
+    }
+
+    // 让该方法在多线程环境下,被异步的调用.
+    @Async
+    public void execute1() {
+        logger.debug("execute1");
+    }
+
+    @Scheduled(initialDelay = 10000, fixedRate = 1000)
+    public void execute2() {
+        logger.debug("execute2");
     }
 }
